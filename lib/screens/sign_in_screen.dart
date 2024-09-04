10937076cdffb24569a764c24a4665e56481fa21
Indrajit Kardile <indrajitflutter2024@gmail.com>
2024-09-04 23:34:44 +0530

    changes added

--- lib/screens/sign_in_screen.dart
+++ lib/screens/sign_in_screen.dart
@@ -22,144 +22,146 @@ class _SignInScreenState extends State<SignInScreen> {
         await prefs.setBool('isLoggedIn', true);
         Navigator.pushReplacementNamed(context, '/dashboard');
       } else {
-        alertMessage(context, "Email can not find");
+        alertMessage(context, "Email can not find Pleaser Sign up first");
       }
     }
   }
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      body: SingleChildScrollView(
-        child: Padding(
-          padding: const EdgeInsets.all(16.0),
-          child: Form(
-            key: _formKey,
-            child: Column(
-              mainAxisAlignment: MainAxisAlignment.center,
-              crossAxisAlignment: CrossAxisAlignment.start,
-              children: [
-                const Text(
-                  "Log In",
-                  style: TextStyle(fontSize: 30),
-                ),
-                const SizedBox(
-                  height: 50,
-                ),
-                TextFormField(
-                  decoration: InputDecoration(
-                      hintText: "Please Enter Email",
-                      labelText: 'Email',
-                      border: OutlineInputBorder(
-                        borderRadius: BorderRadius.circular(8),
-                      )),
-                  keyboardType: TextInputType.emailAddress,
-                  validator: (value) {
-                    if (value!.isEmpty || !value.contains('@')) {
-                      return 'Please enter a valid email';
-                    }
-                    return null;
-                  },
-                  onChanged: (value) => _email = value,
-                ),
-                const SizedBox(height: 10),
-                TextFormField(
-                  decoration: InputDecoration(
-                      hintText: "Please Enter Password",
-                      labelText: 'Password',
-                      border: OutlineInputBorder(
-                        borderRadius: BorderRadius.circular(8),
-                      )),
-                  obscureText: true,
-                  validator: (value) {
-                    if (value!.isEmpty || value.length < 6) {
-                      return 'Password must be at least 6 characters';
-                    }
-                    return null;
-                  },
-                  onChanged: (value) => _password = value,
-                ),
-                Align(
-                  alignment: Alignment.centerRight,
-                  child: TextButton(
-                    onPressed: () {
-                      // Implement Forgot Password Functionality
-                    },
-                    child: const Text('Forgot Password?'),
+    return SafeArea(
+      child: Scaffold(
+        body: SingleChildScrollView(
+          child: Padding(
+            padding: const EdgeInsets.all(16.0),
+            child: Form(
+              key: _formKey,
+              child: Column(
+                mainAxisAlignment: MainAxisAlignment.center,
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: [
+                  const Text(
+                    "Log In",
+                    style: TextStyle(fontSize: 30),
                   ),
-                ),
-                const SizedBox(height: 20),
-                SizedBox(
-                  width: double.infinity,
-                  child: ElevatedButton(
-                    onPressed: _signIn,
-                    style: ElevatedButton.styleFrom(backgroundColor: Colors.blueAccent),
-                    child: const Text(
-                      'Sign In',
-                      style: TextStyle(color: Colors.white),
+                  const SizedBox(
+                    height: 50,
+                  ),
+                  TextFormField(
+                    decoration: InputDecoration(
+                        hintText: "Please Enter Email",
+                        labelText: 'Email',
+                        border: OutlineInputBorder(
+                          borderRadius: BorderRadius.circular(8),
+                        )),
+                    keyboardType: TextInputType.emailAddress,
+                    validator: (value) {
+                      if (value!.isEmpty || !value.contains('@')) {
+                        return 'Please enter a valid email';
+                      }
+                      return null;
+                    },
+                    onChanged: (value) => _email = value,
+                  ),
+                  const SizedBox(height: 10),
+                  TextFormField(
+                    decoration: InputDecoration(
+                        hintText: "Please Enter Password",
+                        labelText: 'Password',
+                        border: OutlineInputBorder(
+                          borderRadius: BorderRadius.circular(8),
+                        )),
+                    obscureText: true,
+                    validator: (value) {
+                      if (value!.isEmpty || value.length < 6) {
+                        return 'Password must be at least 6 characters';
+                      }
+                      return null;
+                    },
+                    onChanged: (value) => _password = value,
+                  ),
+                  Align(
+                    alignment: Alignment.centerRight,
+                    child: TextButton(
+                      onPressed: () {
+                        // Implement Forgot Password Functionality
+                      },
+                      child: const Text('Forgot Password?'),
                     ),
                   ),
-                ),
-                const SizedBox(height: 20),
-                const Center(child: Text('or sign in with')),
-                const SizedBox(height: 10),
-                Row(
-                  mainAxisAlignment: MainAxisAlignment.center,
-                  children: [
-                    // Social buttons with rounded corners
-                    ElevatedButton(
-                      onPressed: () {},
-                      style: ElevatedButton.styleFrom(shape: const StadiumBorder()),
-                      child: Row(
-                        mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                        children: [
-                          SizedBox(
-                              height: 25,
-                              width: 25,
-                              child: Image.asset(
-                                "assets/icon/google.png",
-                                fit: BoxFit.contain,
-                              )),
-                          const SizedBox(
-                            width: 5,
-                          ),
-                          const Text('Google'),
-                        ],
+                  const SizedBox(height: 20),
+                  SizedBox(
+                    width: double.infinity,
+                    child: ElevatedButton(
+                      onPressed: _signIn,
+                      style: ElevatedButton.styleFrom(backgroundColor: Colors.blueAccent),
+                      child: const Text(
+                        'Sign In',
+                        style: TextStyle(color: Colors.white),
                       ),
                     ),
-                    const SizedBox(width: 10),
-                    ElevatedButton(
-                      onPressed: () {},
-                      style: ElevatedButton.styleFrom(shape: const StadiumBorder()),
-                      child: Row(
-                        mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                        children: [
-                          SizedBox(
-                              height: 25,
-                              width: 25,
-                              child: Image.asset(
-                                "assets/icon/facebook.png",
-                                fit: BoxFit.contain,
-                              )),
-                          const SizedBox(
-                            width: 5,
-                          ),
-                          const Text('Facebook'),
-                        ],
-                      ),
-                    ),
-                  ],
-                ),
-                const SizedBox(height: 20),
-                Center(
-                  child: TextButton(
-                    onPressed: () {
-                      Navigator.pushNamed(context, '/signup');
-                    },
-                    child: const Text('Create an Account'),
                   ),
-                ),
-              ],
+                  const SizedBox(height: 20),
+                  const Center(child: Text('or sign in with')),
+                  const SizedBox(height: 10),
+                  Row(
+                    mainAxisAlignment: MainAxisAlignment.center,
+                    children: [
+                      // Social buttons with rounded corners
+                      ElevatedButton(
+                        onPressed: () {},
+                        style: ElevatedButton.styleFrom(shape: const StadiumBorder()),
+                        child: Row(
+                          mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                          children: [
+                            SizedBox(
+                                height: 25,
+                                width: 25,
+                                child: Image.asset(
+                                  "assets/icon/google.png",
+                                  fit: BoxFit.contain,
+                                )),
+                            const SizedBox(
+                              width: 5,
+                            ),
+                            const Text('Google'),
+                          ],
+                        ),
+                      ),
+                      const SizedBox(width: 10),
+                      ElevatedButton(
+                        onPressed: () {},
+                        style: ElevatedButton.styleFrom(shape: const StadiumBorder()),
+                        child: Row(
+                          mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                          children: [
+                            SizedBox(
+                                height: 25,
+                                width: 25,
+                                child: Image.asset(
+                                  "assets/icon/facebook.png",
+                                  fit: BoxFit.contain,
+                                )),
+                            const SizedBox(
+                              width: 5,
+                            ),
+                            const Text('Facebook'),
+                          ],
+                        ),
+                      ),
+                    ],
+                  ),
+                  const SizedBox(height: 20),
+                  Center(
+                    child: TextButton(
+                      onPressed: () {
+                        Navigator.pushNamed(context, '/signup');
+                      },
+                      child: const Text('Create an Account'),
+                    ),
+                  ),
+                ],
+              ),
             ),
           ),
         ),
